73600e9790cf997cdd743825b9123442fd7715bc
RedempleDev <redemple.marcelo@redcoresolutions.com>
2024-03-20 21:57:18 +0800

    remove middle name

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -30,7 +30,7 @@ class UpdateUserRequest extends FormRequest
             'judge_no' => ['numeric'],
             'email' => ['required', 'email', 'string', 'max:255', Rule::unique('users')->ignore($this->route('user'))],
             'password' => ['required', 'min:6', 'max:16', 'string', 'confirmed'],
-            'is_active' => ['boolean'],
+            'is_active' => ['required'],
             'category_id' => ['required', 'exists:categories,id'],
             'role_id' => ['required', 'exists:roles,id'],
             'description' => ['nullable']
